b6a6365c75495a7090394fb137e9ee08418c7318
Doug <djmacdonald@uchicago.edu>
2015-03-14 10:43:14 -0400

    user show now takes user from token auth instead of from url

--- app/js/user_show.js
+++ app/js/user_show.js
@@ -3,18 +3,38 @@
 
 var users = (function(module){
 
-  module.findUser = function(){
+  module.findUser = function(callback){
+    var user;
     if (localStorage.authToken){
       module.authToken = localStorage.authToken;
     }
-
+    $.ajaxPrefilter(function( options ) {
+        options.headers = {};
+        options.headers['AUTHORIZATION'] = module.authToken;
+    });
+    $.ajax({
+      url: users.user_path
+    }).done(function(data){
+      module.user = data;
+      callback(data);
+    }).fail();
   };
 
   module.show_user_page = function(){
     users.$container.empty();
+    $.ajaxPrefilter(function( options ) {
+        options.headers = {};
+        options.headers['AUTHORIZATION'] = module.authToken;
+    });
+    module.findUser(module.renderUser);
+  };
+
+  module.renderUser = function(data){
     $.ajax({
-      url: module.user_path + "/" + environment.getParams()
-    }).done(module.render).fail();
+        url: module.user_path + "/" + data.id
+      }).done(function(response){
+        module.render(response)
+      }).fail();
   };
 
   module.render = function(response){
