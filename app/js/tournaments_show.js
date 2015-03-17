45875fdac8da98c7a24101f9ac8a71ae68040ce2
Helen Thi <ht5658@gmail.com>
2015-03-17 16:54:56 -0400

    added tournament algorithm

--- app/js/tournaments_show.js
+++ app/js/tournaments_show.js
@@ -10,7 +10,7 @@ var tournaments = (function(module){
   };
 
   module.showTournament = function(){
-    module.getTournament(74);
+    module.getTournament(76);
     };
 
   module.getTournament = function(id){
@@ -22,6 +22,7 @@ var tournaments = (function(module){
       if (response.status === 'open'){
         // module.renderOpenTournament(response);
         module.setup();
+        module.runTournament();
       } else {
         module.renderClosedTournament(response);
       }
@@ -34,17 +35,44 @@ var tournaments = (function(module){
 
   module.setup = function(){
     module.array = module.active.images;
-    module.array = module.each_slice(module.array);
   };
   // declaring an array of images in the module.array variable. Then the second time around, we split the array into arrays of 2 objects within each array.
   //  i.e. arr = [1,2,3,4] --> [[1,2]], [[3,4]]
 
-  module.renderOpenTournament = function(response){
-    var activeObject = {question: response.question, leftImage: response.images[0], rightImage: response.images[1]}
+  module.runTournament = function(){
+    module.count = 0;
+    module.array = _.flatten(module.array);
+    module.array = _.shuffle(module.array);
+    module.array = module.each_slice(module.array);
+    module.renderOpenTournament();
+
+  };
+
+  module.renderOpenTournament = function(){
+    if(module.array[module.count].length < 2){
+      module.runTournament();
+    }
+    else{
+      module.renderPairing();
+    }
+  };
+
+  module.renderPairing = function(){
+    var activeObject = {question: module.active.question, leftImage: module.array[module.count][0], rightImage: module.array[module.count][1]};
     var template = Handlebars.compile($('#showOpenTournamentTemplate').html());
     $('#container').html(template({
       tournament: activeObject
     }));
+    $('#leftImage').click(function(){
+      module.array[module.count].pop();
+      module.count += 1;
+      module.renderOpenTournament();
+    });
+    $('#rightImage').click(function(){
+      module.array[module.count].shift();
+      module.count += 1;
+      module.renderOpenTournament();
+    });
   };
 
   module.renderClosedTournament = function(response){
