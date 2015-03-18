003f993493c34eb32c691d17f7e6b19f124c5344
Helen Thi <ht5658@gmail.com>
2015-03-18 10:07:59 -0400

    added comments form to closed tournament

--- app/js/comment_create.js
+++ app/js/comment_create.js
@@ -3,6 +3,25 @@
 
 var comments = (function(module){
 
+  module.submitComment = function(){
+   $('#newComment').submit(module.createComment);
+  };
+
+  module.createComment = function(){
+    $.ajax({
+      url: module.tournament_path,
+      type: 'POST',
+      data: {
+        comment: {
+          tournament_id: tournaments.tournament.id,
+          body: $('#comment').val()
+        }
+      }
+    }).done(function(data){
+      console.log(data);
+    }).fail();
+  };
+
   return module
 
 })(comments || {});
